4cd52327973ccd700666296ad81dd0ae25bdbc60
Developer 23 <alkharboush@hotmail.com>
2019-08-26 11:12:05 +0300

    Add files via upload

--- week 1.js
+++ week 1.js
@@ -1,3 +1,3 @@
 var a = 10;
-console.log("Hello, this is my first challenge and I will do it " + a + " times.");
-console.log("I will do it, I will learn JavaScript.");
+document.write("Hello, this is my first challenge and I will do it " + a + " times.");
+document.write("I will do it, I will learn JavaScript.");
